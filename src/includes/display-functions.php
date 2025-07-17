ab45da9e2191f821e8e258d1b8eeee95851b4d7b
Mike Thicke <mikethicke@gmail.com>
2025-07-17 11:05:53 -0400

    Embed excerpts don't include collection breadcrumbs.

--- src/includes/display-functions.php
+++ src/includes/display-functions.php
@@ -53,8 +53,8 @@ function mobject_excerpt_length($length)
 function mobject_collection_breadcrumbs($content, $fields = false)
 {
     $post = get_queried_object();
-
-    if (!$post || !in_the_loop() || !is_main_query()) {
+    xdebug_break();
+    if (!$post || !in_the_loop() || !is_main_query() || is_embed()) {
         return $content;
     }
 
