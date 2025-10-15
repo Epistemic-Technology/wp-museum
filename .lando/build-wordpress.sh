6ac3501030b354b401d5357a490c235a5e34e9d0
Mike Thicke <mikethicke@gmail.com>
2025-10-15 11:06:22 -0400

    Build / local dev environment updates.

--- .lando/build-wordpress.sh
+++ .lando/build-wordpress.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 # Download latest WordPress release and extract it
+echo "Downloading latest WordPress release..."
 cd /app
 curl -O https://wordpress.org/latest.tar.gz
 tar -xzf latest.tar.gz
 rm latest.tar.gz
 
+echo "Relinking files..."
 rm -f /app/wordpress/wp-config.php && ln -s /app/.lando/wp-config.php /app/wordpress/wp-config.php
 rm -f /app/wordpress/phpinfo.php && ln -s /app/.lando/phpinfo.php /app/wordpress/phpinfo.php
 rm -rf /app/wordpress/wp-content/plugins/wp-museum && ln -s /app /app/wordpress/wp-content/plugins/wp-museum
